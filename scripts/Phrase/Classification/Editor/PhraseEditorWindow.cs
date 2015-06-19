664471f4f2093974b186b89cc5296cf263b85e01
Shiyu <sw673@cornell.edu>
2015-06-19 17:54:10 -0400

    <Japanese IME> [Update] enable hotkey selection for plain text, context and wildcard [Modification] Removes o and x for selecting cursor [Enhancement] enables arrowkeys for cursor movement. [Enhancement] enables Space and Uppercase letters [Add] Conjugates now pops up for verbs after they are selected.

--- scripts/Phrase/Classification/Editor/PhraseEditorWindow.cs
+++ scripts/Phrase/Classification/Editor/PhraseEditorWindow.cs
@@ -16,9 +16,17 @@ public class PhraseEditorWindow : EditorWindow {
 	DictionaryDataEntry[] OnDisplay = new DictionaryDataEntry[displayLines];
 	int offset = 0;
 	DictionaryDataEntry[] currentList = new DictionaryDataEntry[0];
+
+	PhraseSequenceElement[] conjugateDisplay = new PhraseSequenceElement[displayLines];
+	int conjugateOffset = 0;
+	PhraseSequenceElement[] currentConjugations = new PhraseSequenceElement[0];
+
+	private bool focusSwitch = false;
+	private bool onConjugate = false;
+	private DictionaryDataEntry toConjugate = null;
 	/** END *****************************************************************/
 
-
+	bool keepFocus = true;
 
 	PhraseSequence phraseSequence = new PhraseSequence();
 	string lastImeText = "";
@@ -37,42 +45,46 @@ public class PhraseEditorWindow : EditorWindow {
 		var window = GetWindow<PhraseEditorWindow>();
 		window.phraseSequence = phraseSequence;
 	}
-	
+
+	void Awake(){
+		keepFocus = true;
+	}
 	//public override void OnGUI (Rect rect)
 	void OnGUI()
-	{
-		Focus ();
+	{	
+		if(keepFocus)
+			Focus ();
 
-		//GUILayout.BeginArea (rect);
 		EditorGUILayout.BeginHorizontal ();
 		EditorGUILayout.BeginVertical();
 
-
 		imeText = DrawPhrase (phraseSequence, imeText);
-
-		if (selected >= phraseSequence.PhraseElements.Count) {
-			//DrawFilteredWords (phraseSequence);
+		if (onConjugate) {
+			if(toConjugate == null){
+				DrawTenWordForms (selected);
+			}
+			else
+				DrawTenWordForms(toConjugate, false, selected);
+		}
+		else if (selected >= phraseSequence.PhraseElements.Count) {
 			DrawTenFileterWords(phraseSequence, offset);
 		} 
 		else if (phraseSequence.PhraseElements [selected].IsDictionaryWord) {
-			DrawWordForms (selected);
+			onConjugate = true;
+			DrawTenWordForms (selected);
 		} 
 		else {
 			DrawSlotEditor(selected);
 		}
 
 		EditorGUILayout.EndVertical ();
-		if (GUILayout.Button ("Confirm", GUILayout.Width (64f), GUILayout.ExpandHeight (false))) {
-			Close();
-		}
-
 		EditorGUILayout.EndHorizontal ();
 
-		if (lastImeText != imeText) {
+		/** keyBorad event loop ****************************************/
+		HandleKeyboardEvents ();
+	}
 
-		}
-
-		//keyBorad event loop
+	private void HandleKeyboardEvents(){
 		bool canRead = true;
 		if(Event.current.type == EventType.Repaint){
 			canRead = true;
@@ -82,55 +94,175 @@ public class PhraseEditorWindow : EditorWindow {
 		}
 		if(canRead){
 			if (Event.current.type == EventType.KeyDown) {
-				//listen to key event
+				//listen to key events
 				KeyCode key = Event.current.keyCode;
 				ListenToScroll(key);
 				ListenToTyping(key);
+				ListenToControl(key);
 			}
 			//reset entry when typing
 			if(lastImeText != imeText){
 				selected = phraseSequence.PhraseElements.Count;
-				lastImeText = imeText;
-				offset = 0;
+				//lastImeText = imeText;
+				//offset = 0;
 			}
 		}
-		//end of keyBoard event loop
 	}
+	/**    IME UI KeyBoard Event Handling *******************************************************/
 
+	//manage control keys on the window
+	void ListenToControl(KeyCode key){
+		if (key == KeyCode.Escape) {
+			if(focusSwitch)
+				keepFocus = false;
+		}
+		if (key == KeyCode.Return && imeText == "") {
+			Close();
+		}
+	}
 	
 	//managing response to keyboard entries about inputs
 	void ListenToTyping(KeyCode key){
+		//deleting text
 		if (key == KeyCode.Backspace || key == KeyCode.Delete) {
-			imeText = imeText.Substring(0, Mathf.Max(0, imeText.Length - 1));
-		}
-		if(key.ToString().Length == 1 && IsAlphabet(key.ToString()[0])){
-			imeText += key.ToString().ToLower();
-		}
-		int index;
-		if(IsNumeric(key.ToString(), out index)){
-			if(index >= 0 && index < OnDisplay.Length && OnDisplay[index] != null){
-				phraseSequence.PhraseElements.Insert (cursorPosition, new PhraseSequenceElement(OnDisplay[index].ID, 0));
-				imeText = "";
-				cursorPosition = phraseSequence.PhraseElements.Count;
+			//if no imetext, delete the entry in front of cursor
+			if(imeText == "" && cursorPosition != 0){
+				setCursorPosition(cursorPosition - 1);
+				phraseSequence.RemoveAt(cursorPosition);
 			}
+			//if there is imetext, delete one letter from it
+			else{
+				onConjugate = false;
+				toConjugate = null;
+				imeText = imeText.Substring(0, Mathf.Max(0, imeText.Length - 1));
+			}
+		}
+		//entering letters into imeText
+		if(key.ToString().Length == 1 && IsAlphabet(key.ToString()[0])){
+			//set onConjugate and toConjugate as such for every action that will change imeText
+			onConjugate = false;
+			toConjugate = null;
+			if(Event.current.shift){
+				imeText += key.ToString().ToUpper();
+			}
+			else
+				imeText += key.ToString().ToLower();
+		}
+		//enter space into imeText
+		if (key == KeyCode.Space) {
+			onConjugate = false;
+			toConjugate = null;
+			imeText += " ";
+		}
+		/** Potential key strokes that can be used for entering into text ***/
+		//TODO maybe enable punctuations
+		/********************************************************************/
+
+		/** selection key strokes *****/
+		int index;
+
+		//conjugate words selection when modifying existing verbs
+		if (IsNumeric (key.ToString (), out index) 
+		    &&  onConjugate 
+		    && selected < phraseSequence.PhraseElements.Count
+		    && phraseSequence.PhraseElements [selected].IsDictionaryWord) {
+			if(index >= 0 && index < conjugateDisplay.Length && conjugateDisplay[index] != null){
+				phraseSequence.UpdateAt(selected, conjugateDisplay[index]);
+			}
+		}
+
+		else if(IsNumeric(key.ToString(), out index) && imeText != ""){
+			//conjugate words selection for newly typed words
+			if(onConjugate){
+				if(index >= 0 && index < conjugateDisplay.Length && conjugateDisplay[index] != null){
+					phraseSequence.PhraseElements.Insert (cursorPosition, conjugateDisplay[index]);
+					imeText = "";
+					toConjugate = null;
+					onConjugate = false;
+					setCursorPosition(cursorPosition + 1);
+				}
+			}
+			//Japanese word selection
+			else{
+				if(index >= 0 && index < OnDisplay.Length && OnDisplay[index] != null){
+					if(hasConjugate(OnDisplay[index])){
+						toConjugate = OnDisplay[index];
+						Debug.Log (toConjugate);
+						onConjugate = true;
+					}
+					else{
+						phraseSequence.PhraseElements.Insert (cursorPosition, new PhraseSequenceElement(OnDisplay[index].ID, 0));
+						imeText = "";
+						setCursorPosition(cursorPosition + 1);
+					}
+				}
+			}
+		}
+
+		/**   HotKeys for selection when imeText isn't empty  ********************/
+		//TODO maybe change hotkey to ctrl
+		else if (key == KeyCode.Comma && imeText != "") {
+			var element = new PhraseSequenceElement (PhraseSequenceElementType.Text, imeText);
+			//phrase.Add (new PhraseSequenceElement(PhraseSequenceElementType.Text, imeText));
+			imeText = "";
+			phraseSequence.PhraseElements.Insert (cursorPosition, element);
+			setCursorPosition(cursorPosition + 1);
+		}
+		else if (key == KeyCode.Period && imeText != "") {
+			var element = new PhraseSequenceElement (PhraseSequenceElementType.ContextSlot, imeText);
+			//phrase.Add (new PhraseSequenceElement(PhraseSequenceElementType.Text, imeText));
+			imeText = "";
+			phraseSequence.PhraseElements.Insert (cursorPosition, element);
+			setCursorPosition(cursorPosition + 1);
+		}
+		else if (key == KeyCode.Slash && imeText != "") {
+			var element = new PhraseSequenceElement (PhraseSequenceElementType.Wildcard, imeText);
+			//phrase.Add (new PhraseSequenceElement(PhraseSequenceElementType.Text, imeText));
+			imeText = "";
+			phraseSequence.PhraseElements.Insert (cursorPosition, element);
+			setCursorPosition(cursorPosition + 1);
 		}
 	}
 
 	//managing response to keyboard entries about scrolling
 	void ListenToScroll(KeyCode key) {
 		if(key == nextPage){
-			offset = Mathf.Min(offset + displayLines, currentList.Length - displayLines);
+			if(imeText == ""){
+				setCursorPosition(cursorPosition + 1);
+			}
+			else if (onConjugate){
+				conjugateOffset = Mathf.Min(conjugateOffset + displayLines, currentConjugations.Length - displayLines);
+			}
+			else
+				offset = Mathf.Min(offset + displayLines, currentList.Length - displayLines);
 		}
 		if(key == prevPage){
-			offset = Mathf.Max(0, offset - displayLines);
+			if(imeText == ""){
+				setCursorPosition(cursorPosition - 1);
+			}
+			else if (onConjugate){
+				conjugateOffset = Mathf.Max(0, conjugateOffset - displayLines);
+			}
+			else
+				offset = Mathf.Max(0, offset - displayLines);
 		}
 		if(key == nextEntry){
-			offset = Mathf.Min(offset + 1, currentList.Length - displayLines);
+			if(onConjugate){
+				conjugateOffset = Mathf.Min(conjugateOffset + 1, currentConjugations.Length  - displayLines);
+			}
+			else
+				offset = Mathf.Min(offset + 1, currentList.Length - displayLines);
 		}
 		if(key == prevEntry){
-			offset = Mathf.Max(0, offset - 1);
+			if(onConjugate){
+				conjugateOffset = Mathf.Max(0, conjugateOffset - 1);
+			}
+			else
+				offset = Mathf.Max(0, offset - 1);
 		}
 	}
+
+	/** IME UI helper ****************************************************************************/
 
 	//key input helpers
 	bool IsAlphabet(char c){
@@ -154,58 +286,93 @@ public class PhraseEditorWindow : EditorWindow {
 		index = -1;
 		return false;
 	}
-	//end of key input helpers
 
+	void setCursorPosition(int newPos){
+		if (newPos < 0)
+			newPos = 0;
+		if (newPos >= phraseSequence.PhraseElements.Count)
+			newPos = phraseSequence.PhraseElements.Count;
+		cursorPosition = newPos;
+	}
+
+	private void createTextField(){
+		//TODO highlight text entry area
+//		Color oldColor = GUI.color;
+//		GUI.color = Color.blue;
+		GUILayout.Label (imeText, GUILayout.ExpandWidth(false), GUILayout.MinWidth(20));
+//		GUI.color = oldColor;
+		if(imeText != lastImeText){
+			lastImeText = imeText;
+			offset = 0;
+		}
+	}
+	/** end of key input helpers **/
+
+	/** UI Draw Functions **************************************************************************/
 	public string DrawPhrase(PhraseSequence phrase, string imeText){
 		EditorGUILayout.BeginVertical ();
 
-		var s = phrase.Translation;
-		if (s == null) {
-			s = "";
-		}
-		s = EditorGUILayout.TextField ("Translation", s);
-		if (s != "") {
-			phrase.Translation = s;
-		}
+		/** Translation fields **/
+//		var s = phrase.Translation;
+//		if (s == null) {
+//			s = "";
+//		}
+//		GUI.SetNextControlName("translationCtrl");
+//		s = EditorGUILayout.TextField ("Translation", s, GUILayout.ExpandWidth(false), GUILayout.Width(500f));
+//		if (s != "") {
+//			phrase.Translation = s;
+//		}
 
+		/** Focus switch on the window **/
+//		EditorGUILayout.BeginHorizontal ();
+//
+//		//set to focus back on the window
+//		if (GUILayout.Button ("Focus On Window", GUILayout.ExpandWidth (false))) {
+//			GUI.FocusControl(null);
+//			if(focusSwitch)
+//				keepFocus = true;
+//		}
+//		//set to remove focus from the window
+//		if (GUILayout.Button ("Focus Away From Window", GUILayout.ExpandWidth (false))) {
+//			if(focusSwitch)
+//				keepFocus = false;
+//		}
+//
+//		EditorGUILayout.EndHorizontal ();
 		EditorGUILayout.BeginHorizontal ();
 
 		int i = 0;
         for (; i < phrase.PhraseElements.Count; i++) {
             if (cursorPosition == i) {
-				//GUI.SetNextControlName("EntryField");
-				EditorGUILayout.LabelField (imeText);
-                //imeText = EditorGUILayout.TextField(imeText);
-				if(imeText != lastImeText){
-					lastImeText = imeText;
-					offset = 0;
-				}
+
+				createTextField();
             } else {
-                if (GUILayout.Button("o", GUILayout.ExpandWidth(false))) {
-                    cursorPosition = i;
-                }
+//                if (GUILayout.Button("o", GUILayout.ExpandWidth(false))) {
+//					setCursorPosition(i);
+//                }
             }
 
             var word = phrase.PhraseElements[i];
 
             if (GUILayout.Button(word.GetText(), GUILayout.ExpandWidth(false))) {
+				onConjugate = false;
                 selected = i;
             }
 
-            if (GUILayout.Button("x", GUILayout.ExpandWidth(false))) {
-                phrase.RemoveAt(i);
-                break;
-            }
+//            if (GUILayout.Button("x", GUILayout.ExpandWidth(false))) {
+//                phrase.RemoveAt(i);
+//				setCursorPosition(cursorPosition - 1);
+//                break;
+//            }
         }
 
         if(cursorPosition >= phrase.PhraseElements.Count){
-			cursorPosition = phrase.PhraseElements.Count;
-			EditorGUILayout.LabelField (imeText);
-			//imeText = EditorGUILayout.TextField (imeText);
+			setCursorPosition(phrase.PhraseElements.Count);
+			createTextField();
         } else {
-            if(GUILayout.Button("o", GUILayout.ExpandWidth(false))){
-			    cursorPosition = phrase.PhraseElements.Count;
-            }
+//            if(GUILayout.Button("o", GUILayout.ExpandWidth(false))){
+//				setCursorPosition(phrase.PhraseElements.Count);
+//            }
         }
 		
 		EditorGUILayout.EndHorizontal ();
@@ -223,6 +390,48 @@ public class PhraseEditorWindow : EditorWindow {
 				}
 			}
 		}
+	}
+
+	private bool hasConjugate(DictionaryDataEntry e){
+		//e itself is always returned, check if any other form exists
+		return ConjugationTool.GetForms (e).Length > 1;
+	}
+
+	void DrawTenWordForms(int selected){
+		var entry = DictionaryData.Instance.GetEntryFromID (phraseSequence.PhraseElements[selected].WordID);
+		DrawTenWordForms (entry, true, selected);
+	}
+
+	void DrawTenWordForms(DictionaryDataEntry entry, bool update, int selected){
+		currentConjugations = ConjugationTool.GetForms (entry);
+		if (conjugateOffset + displayLines >= currentConjugations.Length)
+			conjugateOffset = Mathf.Max (0, currentConjugations.Length - displayLines);
+		if (conjugateOffset < 0)
+			conjugateOffset = 0;
+		for (int i = 0; i < displayLines; i++) {
+			if (conjugateOffset + i >= currentConjugations.Length || conjugateOffset + i < 0)
+				conjugateDisplay[i] = null;
+			else{
+				var e = currentConjugations [conjugateOffset + i];
+				var label = string.Format ("{0}:   {1}", i + 1, e.GetText());
+				GUIStyle buttonStyle = new GUIStyle();
+				buttonStyle.alignment = TextAnchor.LowerLeft;
+				if (GUILayout.Button (label, buttonStyle)) {
+					if(update){
+						phraseSequence.UpdateAt(selected, e);
+						break;
+					}
+					else{
+						phraseSequence.PhraseElements.Insert (cursorPosition, e);
+						imeText = "";
+						toConjugate = null;
+						break;
+					}
+				}
+				conjugateDisplay [i] = e;
+			}
+		}
+
 	}
 
 	void DrawSlotEditor(int wordIndex){
@@ -267,29 +476,7 @@ public class PhraseEditorWindow : EditorWindow {
 					//phrase.Add (new PhraseSequenceElement(0, 0));
 				}
 			} else {
-				if (GUILayout.Button ("Add as plain text")) {
-					element = new PhraseSequenceElement (PhraseSequenceElementType.Text, imeText);
-					//phrase.Add (new PhraseSequenceElement(PhraseSequenceElementType.Text, imeText));
-					imeText = "";
-				}
-				
-				if (GUILayout.Button ("Add as context data")) {
-					element = new PhraseSequenceElement (PhraseSequenceElementType.ContextSlot, imeText);
-					//phrase.Add (new PhraseSequenceElement(PhraseSequenceElementType.ContextSlot, imeText));
-					imeText = "";
-				}
-				
-				if (GUILayout.Button ("Add as tag")) {
-					element = new PhraseSequenceElement (PhraseSequenceElementType.TaggedSlot, imeText);
-					//phrase.Add(new PhraseSequenceElement(PhraseSequenceElementType.TaggedSlot, imeText));
-					imeText = "";
-				}
-				
-				if (GUILayout.Button ("Add as wildcard")) {
-					element = new PhraseSequenceElement (PhraseSequenceElementType.Wildcard, imeText);
-					//phrase.Add(new PhraseSequenceElement(PhraseSequenceElementType.Wildcard, imeText));
-					imeText = "";
-				}
+
 				currentList = DictionaryData.Instance.FilterEntriesFromRomaji (imeText).ToArray ();
 				if (offset + displayLines >= currentList.Length)
 					offset = Mathf.Max (0, currentList.Length - displayLines);
@@ -300,8 +487,10 @@ public class PhraseEditorWindow : EditorWindow {
 						OnDisplay[i] = null;
 					else{
 						var e = currentList [offset + i];
-						var label = string.Format ("{0} [{1}] {2} ({3}) {4}", i + 1, e.ID, e.Kanji, e.Kana, e.EnglishSummary);
-						if (GUILayout.Button (label)) {
+						string label = string.Format ("{0}:   [{1}] {2} ({3}) {4}", i + 1, e.ID, e.Kanji, e.Kana, e.EnglishSummary);
+						GUIStyle buttonStyle = new GUIStyle();
+						buttonStyle.alignment = TextAnchor.LowerLeft;
+						if (GUILayout.Button (label, buttonStyle)) {
 							element = new PhraseSequenceElement (e.ID, 0);
 							//phrase.Add (new PhraseSequenceElement(e.ID, 0));
 							imeText = "";
@@ -311,7 +500,31 @@ public class PhraseEditorWindow : EditorWindow {
 					}
 				}
 
+				string buttonLabel = string.Format("{0}  [Hotkey: {1}]", "Add as plain text", ",");
+				if (GUILayout.Button (buttonLabel)) {
+					element = new PhraseSequenceElement (PhraseSequenceElementType.Text, imeText);
+					//phrase.Add (new PhraseSequenceElement(PhraseSequenceElementType.Text, imeText));
+					imeText = "";
+				}
+				buttonLabel = string.Format("{0}  [Hotkey: {1}]", "Add as context data", ".");
+				if (GUILayout.Button (buttonLabel)) {
+					element = new PhraseSequenceElement (PhraseSequenceElementType.ContextSlot, imeText);
+					//phrase.Add (new PhraseSequenceElement(PhraseSequenceElementType.ContextSlot, imeText));
+					imeText = "";
+				}
 				
+//				if (GUILayout.Button ("Add as tag")) {
+//					element = new PhraseSequenceElement (PhraseSequenceElementType.TaggedSlot, imeText);
+//					//phrase.Add(new PhraseSequenceElement(PhraseSequenceElementType.TaggedSlot, imeText));
+//					imeText = "";
+//				}
+				buttonLabel = string.Format("{0}  [Hotkey: {1}]", "Add as wildcard", "/");
+				if (GUILayout.Button (buttonLabel)) {
+					element = new PhraseSequenceElement (PhraseSequenceElementType.Wildcard, imeText);
+					//phrase.Add(new PhraseSequenceElement(PhraseSequenceElementType.Wildcard, imeText));
+					imeText = "";
+				}
+
 				if (element != null) {
 					phrase.PhraseElements.Insert (cursorPosition, element);
 				}
@@ -324,7 +537,7 @@ public class PhraseEditorWindow : EditorWindow {
 		} 
 		
 		if (prevCount != phrase.PhraseElements.Count) {
-			cursorPosition = phrase.PhraseElements.Count;
+			setCursorPosition(cursorPosition + 1);
 		}
 	}
 	
