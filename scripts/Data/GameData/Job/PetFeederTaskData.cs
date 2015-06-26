dbe58de23a387cde904148d02855b8052dd053b2
Shiyu <sw673@cornell.edu>
2015-06-26 09:50:24 -0400

    modified class structure for Q and A task data

--- scripts/Data/GameData/Job/PetFeederTaskData.cs
+++ scripts/Data/GameData/Job/PetFeederTaskData.cs
@@ -7,32 +7,10 @@ using System.Reflection;
 using System.Xml.Serialization;
 using System.Linq;
 
-/**
- * An Extension of JobTaskGameData that deals with tasks that have more than one possible answer
- * The answers are grouped into categories and answers in the same category have the same correctness
- */
-public class PetFeederTaskData : QATaskGameData {
-	
-	Dictionary<string, Sprite> answerPictureDictionary;
+
+public class PetFeederTaskData : PictureQATaskData {
 
 	public PetFeederTaskData() : base(){
-		answerPictureDictionary = new Dictionary<string, Sprite> ();
 	}
 
-	new public void AddQA(string question, string answer){
-		base.AddQA (question, answer);
-		answerPictureDictionary.Add (answer, new Sprite ());
-	}
-
-	public void AddQA(string question, string answer, Sprite picture){
-		QALine newline = new QALine (question, answer);
-		QAlist.Add (newline);
-		answerPictureDictionary.Add (answer, picture);
-	}
-
-	public Sprite getPicture(string answer){
-		Sprite ret;
-		answerPictureDictionary.TryGetValue (answer, out ret);
-		return ret;
-	}
 }
---
+++ scripts/Data/GameData/Job/PictureQATaskData.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections; 
+using System.Collections.Generic;
+using System.Reflection;
+using System.Xml.Serialization;
+using System.Linq;
+
+public class PictureQATaskData : QATaskGameData {
+	
+	Dictionary<string, Sprite> answerPictureDictionary;
+
+	public PictureQATaskData() : base(){
+		answerPictureDictionary = new Dictionary<string, Sprite> ();
+	}
+
+	new public void AddQA(string question, string answer){
+		base.AddQA (question, answer);
+		answerPictureDictionary.Add (answer, new Sprite ());
+	}
+
+	public void AddQA(string question, string answer, Sprite picture){
+		QALine newline = new QALine (question, answer);
+		QAlist.Add (newline);
+		answerPictureDictionary.Add (answer, picture);
+	}
+
+	public Sprite getPicture(string answer){
+		Sprite ret;
+		answerPictureDictionary.TryGetValue (answer, out ret);
+		return ret;
+	}
+}
