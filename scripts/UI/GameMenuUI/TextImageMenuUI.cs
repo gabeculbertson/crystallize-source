72e46fdaf16b60393b20b8616e28ac16e39e73fc
Shiyu <sw673@cornell.edu>
2015-06-26 13:24:42 -0400

    Modified TextMenuUI, it now works

--- scripts/UI/GameMenuUI/TextImageMenuUI.cs
+++ scripts/UI/GameMenuUI/TextImageMenuUI.cs
@@ -13,22 +13,22 @@ public class TextImageMenuUI : ImageMenuUI<TextImageItem, TextMenuItemEventArg> 
 	//a local cache of the original text given to the menu
 	//this will not be updated when the shown text is changed
 	//can be used for purposes like hidden answer key
-	private string myText;
 
 	#region implemented abstract members of SelectionMenuUI
 	protected override void InitializeButton (GameObject obj, TextImageItem item)
 	{
 		base.InitializeButton (obj, item);
-		myText = item.text;
-		if(item.showText){
-			Text objText = obj.GetComponentInChildren<Text> ();
-			objText.text = item.text;
+		Text objText = obj.GetComponentInChildren<Text> ();
+		if(!item.showText){
+			objText.gameObject.SetActive(false);
 		}
+		objText.text = item.text;
+
 	}
 
 	protected override TextMenuItemEventArg createEventArg (GameObject obj)
 	{
-		return new TextMenuItemEventArg (myText);
+		return new TextMenuItemEventArg (obj.GetComponentInChildren<Text> ().text);
 	}
 	#endregion
 	
